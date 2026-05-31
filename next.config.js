d30272354dd4949ef2cc119fda0da1cef37411ac
abuabeng <akosuabuabeng@googlemail.com>
2026-05-31 17:57:54 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false, // disabled so useEffect runs once (prototype JS is imperative)
-  output: 'export',       // static export — works on Vercel, Netlify, or any CDN
-}
-
-module.exports = nextConfig
